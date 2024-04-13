0a22b1f058abb7a174e7a97731503d9be64a02ae
AhmedMostafa809 <am2098432@gmail.com>
2024-04-13 18:26:32 +0200

    Initial commit

---
+++ lib/app_constants/app_fonts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+double getResponsiveFontSize(BuildContext context, double fontSize) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  if (width < 400) {
+    return width / 400;
+  } else if (width < 600) {
+    return width / 700;
+  } else {
+    return width / 1000;
+  }
+}
